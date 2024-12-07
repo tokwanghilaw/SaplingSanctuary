e5a88c82de560dc6c3051fd0d986f7c357995d98
tokwanghilaw <billielabrador03@gmail.com>
2024-12-07 16:55:30 +0800

    wow progress

---
+++ My project/Assets/Scripts/BackgroundController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour
+{
+    private float startPos, length;
+    public GameObject cam;
+    public float parallaxEffect; //speed of bg relative to cam
+
+    void Start()
+    {
+        startPos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float distance = cam.transform.position.x * parallaxEffect;
+        // 0 = move with cam | 1 = wont move | 0.5 = half
+        float movement = cam.transform.position.x * (1 - parallaxEffect);
+
+        transform.position = new Vector3(startPos + distance, transform.position.y, transform.position.z);
+    
+        if(movement > startPos + length){
+            startPos += length;
+        }
+        else if (movement < startPos - length)
+        {
+            startPos -= length;
+        }
+    }
+}
